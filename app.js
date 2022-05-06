3c706a7bb9b3a65814b18b69a059bdf57b45bdcf
jcmonnet <jcmonnet0408@gmail.com>
2022-05-06 17:44:26 +0200

    ajout de variables, et light mode fonctionnel!!

--- app.js
+++ app.js
@@ -38,3 +38,23 @@ function carousel() {
   x[myIndex-1].style.display = "block";
   setTimeout(carousel, 2000); 
 }
+
+// lightmode
+var toggle = document.getElementById("lightmode");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+
+
+toggle.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "dark";
+
+    if (currentTheme === "dark") {
+        targetTheme = "light";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
