77672025990d894c45b75a3e109ff87fcadfb852
jcmonnet <jcmonnet0408@gmail.com>
2022-05-07 15:14:03 +0200

    console log de verif

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function carousel() {
 
 // lightmode
 var toggle = document.getElementById("lightmode");
-
+console.log(lightmode);
 var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
 if (storedTheme)
     document.documentElement.setAttribute('data-theme', storedTheme)
