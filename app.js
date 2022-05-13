68568b772a86331e8f951c58484b7a608c21bc1f
jcmonnet <jcmonnet0408@gmail.com>
2022-05-13 11:15:42 +0200

    modif comm

--- app.js
+++ app.js
@@ -39,11 +39,15 @@ function carousel() {
   setTimeout(carousel, 2000); 
 }
 
-// lightmode: Après avoir mis une classe light sur mon body dans le html.
+// lightmode: Après avoir mis une classe light sur mon body.
 // Après avoir dans le css, mis le dark sur mon body par defaut. J'ai mis toutes mes var mode clair dans la classe light.
 // Et dans le js, je pointe vers mon bouton, et lui demande au clic de lancer la fonction change.
 // La fonction change où je déclare la var blanc, qui pointe vers la classe du body. Et la fonction switch/toggle sur la classe light=mode clair
 let couleur=document.getElementById('lightmode');
+/**
+ * premiere ligne
+ * 
+ */
 couleur.addEventListener('click',change);
 function change (){
   let blanc=document.body;
