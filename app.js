4ebe13f2d7a7c53ff45004324a8e007dc5c1d1c5
jcmonnet <jcmonnet0408@gmail.com>
2022-05-22 17:23:00 +0200

    modif section passion, variables css , comm et dossier images

--- app.js
+++ app.js
@@ -7,49 +7,37 @@ cv.addEventListener ('click', apparait);
 function apparait (){
   cvBig.style.display='flex';
 }
-
 // Pour sortir de la modal, click sur background la referme. Même chose en sens inverse.
 cvBig.addEventListener ('click',fermeture);
 function fermeture (){
   cvBig.style.display='none';
 }
 
-// slider: on déclare i comme variable pour partir de cet élément (1ère image). On appelle la fonction carousel.
-//  On déclare x pour selectionner les 4 images.
-// Ensuite on lance une boucle qui de ajouter chaque Image en partant de 0, en les mettant en none/invisibles. Tant que c'est inferieur à 4.
+// menu caché, apparait. Cible btn, event au click => lance deplacement horizontal de 30vw (width bloc)
+const btnHobby = document.getElementById('hobby')
+const bloc = document.querySelector('.cache')
 
-var myIndex = 0;
-carousel();
+btnHobby.addEventListener('click', function () {
+  bloc.classList.toggle('cache')
+})
 
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
-  }
-  
-//  Ensuite on déclare l'index à 1 au lieu de 0 (donc 2e image). La boucle se poursuit. Et par contre avec le index-1, on lui demande
-// de repasser en block/visible l'image qui passe en premiere image (index 1 =2e image, donc index-1 = 1ere image)
-// Enfin avec le settimeout on indique la fin de la boucle. Et sa relance toutes les 2 secondes, d'où le changement d'image toutes les 2 secondes.
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1}
-  // console.log(myIndex);  
-  // console.log(x[myIndex-1]);
-  x[myIndex-1].style.display = "block";
-  setTimeout(carousel, 2000); 
-}
-
-// lightmode: Après avoir mis une classe light sur mon body.
-// Après avoir dans le css, mis le dark sur mon body par defaut. J'ai mis toutes mes var mode clair dans la classe light.
-// Et dans le js, je pointe vers mon bouton, et lui demande au clic de lancer la fonction change.
-// La fonction change où je déclare la var blanc, qui pointe vers la classe du body. Et la fonction switch/toggle sur la classe light=mode clair
+// lightmode: ciblage btn + event au click + switch class sur variables css.
 let couleur=document.getElementById('lightmode');
-/**
- * premiere ligne
- * 
- */
 couleur.addEventListener('click',change);
 function change (){
   let blanc=document.body;
   blanc.classList.toggle('light');
+}
+
+// galerie
+let nature=document.getElementById('liengalerie');
+let decor=document.getElementById('conteneurgalerie');
+nature.addEventListener('click', spawn);
+function spawn () {
+  decor.style.display='block';
+}
+
+decor.addEventListener('click', hidden);
+function hidden () {
+  decor.style.display='none';
 }
